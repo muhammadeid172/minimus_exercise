906c584c40763eaf3dda2e150cadb5aaa5072d01
Muhammad Eid <mueid172@gmail.com>
2026-06-07 02:49:16 +0000

    create files in /build dir & add to gitignore

--- submit/tests/run_tests.sh
+++ submit/tests/run_tests.sh
@@ -15,17 +15,19 @@ esac
 echo "Detected architecture: $ARCH"
 
 echo "=== Cleaning old packages ==="
-rm -rf packages
+rm -rf ./build
+mkdir -p build
+cd ./build
 
 echo "=== Building Melange package ==="
 melange keygen
-melange build --arch $ARCH --signing-key melange.rsa melange/dasel.yaml
+melange build --arch $ARCH --signing-key melange.rsa ../melange/dasel.yaml
 
 echo "=== Testing Melange package ==="
-melange test --arch $ARCH melange/dasel.yaml
+melange test --arch $ARCH ../melange/dasel.yaml
 
 echo "=== Building apko container image ==="
-apko build --arch $ARCH apko/dasel.yaml dasel ./dasel-image-$ARCH.tar
+apko build --arch $ARCH ../apko/dasel.yaml dasel ./dasel-image-$ARCH.tar
 
 echo "=== Loading Docker image ==="
 docker load -i ./dasel-image-$ARCH.tar
@@ -34,9 +36,10 @@ echo "=== Running version command inside container ==="
 docker run --rm dasel:latest-$ARCH version
 
 echo "=== Running normal YAML test inside container ==="
-cat tests/normal.yaml | docker run --rm -i dasel:latest-$ARCH query --in yaml
+cat ../tests/normal.yaml | docker run --rm -i dasel:latest-$ARCH query --in yaml
 
 echo "=== Running malicious YAML test inside container ==="
-cat tests/malicious.yaml | docker run --rm -i dasel:latest-$ARCH query --in yaml
+cat ../tests/malicious.yaml | docker run --rm -i dasel:latest-$ARCH query --in yaml
 
+cd ..
 echo "All tests completed successfully."
